572e790fbf06cde700d829f4b5d9d9cac50e0881
Anna Górska <annagorska.arch@gmail.com>
2023-05-02 12:27:18 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,35 +1,40 @@
 'use strict';
 
 function titleClickHandler(event){
-    console.log('Link was clicked!', event);  
+  const clickedElement = this;
+  console.log('Link was clicked!', event);  
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts .active');
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
-
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-}
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
   }
+
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+
+  /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+
+  /* [IN PROGRESS] add class 'active' to the correct article */
+}
+  
+const links = document.querySelectorAll('.titles a');
+  
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
